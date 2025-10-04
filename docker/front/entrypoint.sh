ba512c6a233bccad3cb89033c7701e5f2f51c25c
MES4game <mes4.game@gmail.com>
2025-10-04 13:44:29 +0200

    fix entrypoint and runtime-config template

--- docker/front/entrypoint.sh
+++ docker/front/entrypoint.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
-eval "cat <<EOF
-$(<"/runtime-config.json.template")
-EOF" > "./runtime-config.json"
+TARGET="./runtime-config.json"
+SOURCE="/runtime-config.json.template"
+rm -f "${TARGET}"
+touch "${TARGET}"
+while IFS= read -r line; do
+    eval "echo \"${line}\"" >> "${TARGET}"
+done < "${SOURCE}"
 
 nginx -g 'daemon off;'
